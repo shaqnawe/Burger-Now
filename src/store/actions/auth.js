d15fe681cba83ccd49b98e0f6608f44cdf353d1d
shaqnawe <shaktishah40@gmail.com>
2022-03-28 17:29:31 -0500

    added env file

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -48,9 +48,11 @@ export const auth = (email, password, isSignup) => {
             password: password,
             returnSecureToken: true
         };
-        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBoeUKZA8VFivkUOIIjHrttcb634ERE9Qs';
+        let url =
+          `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.REACT_APP_FIREBASE_API_KEY}`;
         if(!isSignup)
-        url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBoeUKZA8VFivkUOIIjHrttcb634ERE9Qs';
+        url =
+          `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.REACT_APP_FIREBASE_API_KEY}`;
         axios.post(url, authData)
         .then(response => {
             const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
